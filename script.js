a1a54f2b68adda359658fa620f7cc486428bc137
Aiat <aziatskitigr98@gmail.com>
2024-10-31 02:55:31 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const themeToggle = document.getElementById('theme-toggle');
-    const body = document.body;
+let slideIndex = 1;
+showSlides(slideIndex);
 
-    // Проверяем, сохранена ли тема в localStorage
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme) {
-        body.classList.add(savedTheme); // Применяем сохранённую тему
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("slides");
+    const dots = document.getElementsByClassName("dot");
+    
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
     }
+    
+    for (let j = 0; j < dots.length; j++) {
+        dots[j].className = dots[j].className.replace(" active", "");
+    }
+    
+    slides[slideIndex - 1].style.display = "block";  
+    dots[slideIndex - 1].className += " active";
+}
 
-    // Обработчик события для кнопки смены темы
-    themeToggle.addEventListener('click', () => {
-        if (body.classList.contains('dark')) {
-            body.classList.remove('dark');
-            body.classList.add('default');
-            localStorage.setItem('theme', 'default'); // Сохраняем выбранную тему
-        } else {
-            body.classList.remove('default');
-            body.classList.add('dark');
-            localStorage.setItem('theme', 'dark'); // Сохраняем выбранную тему
-        }
-    });
-});
+// Автоматическая смена слайдов
+setInterval(function() {
+    plusSlides(1);
+}, 5000);

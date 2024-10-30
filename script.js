3e1ee8c3c3841f285f00bdf769ba982432eb8173
Aiat <aziatskitigr98@gmail.com>
2024-10-31 03:32:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeToggle = document.getElementById('theme-toggle');
     const body = document.body;
 
-    // Проверяем, сохранена ли тема в localStorage
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme) {
-        body.classList.add(savedTheme); // Применяем сохранённую тему
+        body.classList.add(savedTheme);
     }
 
-    // Обработчик события для кнопки смены темы
     themeToggle.addEventListener('click', () => {
         if (body.classList.contains('dark')) {
             body.classList.remove('dark');
-            localStorage.setItem('theme', 'default'); // Сохраняем выбранную тему
+            localStorage.setItem('theme', 'default');
         } else {
             body.classList.add('dark');
-            localStorage.setItem('theme', 'dark'); // Сохраняем выбранную тему
+            localStorage.setItem('theme', 'dark');
         }
     });
 });
-
-

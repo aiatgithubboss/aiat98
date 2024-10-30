3495afa5a29b834eca35d046decf9869e386b2ee
Aiat <aziatskitigr98@gmail.com>
2024-10-31 03:13:24 +0600

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,39 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("slides");
+    const dots = document.getElementsByClassName("dot");
+    
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    
+    for (let j = 0; j < dots.length; j++) {
+        dots[j].className = dots[j].className.replace(" active", "");
+    }
+    
+    slides[slideIndex - 1].style.display = "block";  
+    dots[slideIndex - 1].className += " active";
+}
+
+// Автоматическая смена слайдов
+setInterval(function() {
+    plusSlides(1);
+}, 5000);

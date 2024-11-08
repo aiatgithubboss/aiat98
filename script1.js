56380359554949b98a9ac0ba27448c72ae51ec21
Aiat <aziatskitigr98@gmail.com>
2024-11-09 02:21:31 +0600

    Update script1.js

--- script1.js
+++ script1.js
@@ -13,3 +13,13 @@ document.getElementById('registration-form').addEventListener('submit', function
         // Например, используя fetch или XMLHttpRequest
     }
 });
+
+// script.js
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('mouseenter', () => {
+        card.style.transform = 'rotateY(20deg) rotateX(15deg)';
+    });
+    card.addEventListener('mouseleave', () => {
+        card.style.transform = 'rotateY(0deg) rotateX(0deg)';
+    });
+});
